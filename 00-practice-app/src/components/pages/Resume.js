0a7817d7ef8123b2db8e16b327d697d7edfce7c9
BenC <bcuttance1998@gmail.com>
2023-08-07 15:44:07 +0930

    adding file download

--- 00-practice-app/src/components/pages/Resume.js
+++ 00-practice-app/src/components/pages/Resume.js
@@ -1,62 +1,69 @@
 import React from 'react';
 
 const frontEndSkills = [
-    { frontEnd: "HTML (Hypertext Markup Language)" },
-    { frontEnd: "CSS (Cascading Style Sheets)" },
-    { frontEnd: "JavaScript" },
-    { frontEnd: "React" },
-    { frontEnd: "Webpack" },
-  ]
-  
-  const backEndSkills = [
-    { BackEnd: "Node.js" },
-    { BackEnd: "MySQK" },
-    { BackEnd: "MongoDB" },
-    { BackEnd: "Express,js" },
-    { BackEnd: 'Restful API\'s' },
-    { BackEnd: 'OAuth' },
-  ]
-  
-  function FrontEnd({ skills }) {
-    return (
-      <ul>
-        <li>
-          {skills.frontEnd}
-        </li>
-      </ul>
-    )
-  }
-  
-  function BackEnd({ skills }) {
-    return (
-      <ul>
-        <li>
-          {skills.BackEnd}
-        </li>
-      </ul>
-    )
-  }
+  { frontEnd: "HTML (Hypertext Markup Language)" },
+  { frontEnd: "CSS (Cascading Style Sheets)" },
+  { frontEnd: "JavaScript" },
+  { frontEnd: "React" },
+  { frontEnd: "Webpack" },
+]
 
-const resumeFile = 'http://localhost:3000/resumeFile_md.pdf' 
+const backEndSkills = [
+  { BackEnd: "Node.js" },
+  { BackEnd: "MySQK" },
+  { BackEnd: "MongoDB" },
+  { BackEnd: "Express,js" },
+  { BackEnd: 'Restful API\'s' },
+  { BackEnd: 'OAuth' },
+]
+
+function FrontEnd({ skills }) {
+  return (
+    <ul>
+      <li>
+        {skills.frontEnd}
+      </li>
+    </ul>
+  )
+}
+
+function BackEnd({ skills }) {
+  return (
+    <ul>
+      <li>
+        {skills.BackEnd}
+      </li>
+    </ul>
+  )
+}
+
+const resumeFile = 'http://localhost:3000/#resume/resumeFile_pdf.pdf'
+
 
 export default function Resume() {
   const downloadFileAtURL = (url) => {
+    fetch(url).then(response => response.blob().then(blob => {
+      const blobURL = window.URL.createObjectURL(new Blob([blob]))
+      const aTag = document.createElement('a')
+      aTag.href = blobURL
 
-    const aTag = document.createElement('a')
-    aTag.href = url
-    aTag.setAttribute('download', 'resumeFile_md.pdf')
-    document.body.appendChild(aTag)
-    aTag.click()
-    aTag.remove();
+      aTag.setAttribute('download', 'resumeFile_pdf.pdf')
+      document.body.appendChild(aTag)
+      aTag.click()
+      aTag.remove();
+
+    }))
+
+
   }
-  
+
   return (
     <div >
       <h1 className='pageTitle'>About Page</h1>
       <div className="card card text-bg-light mb-3 card-body" style={{ margin: '20px' }}>
         <h2 className="card-title" style={{ lineHeight: '2' }}>Skill set</h2>
-        <h6>Download my <a onClick={()=> {downloadFileAtURL(resumeFile)}} href='#resume' >resume here</a>
-        </h6>  
+        <h6>Download my <a onClick={() => { downloadFileAtURL(resumeFile) }} href='#resume' >resume here</a>
+        </h6>
         <h5> Course Work </h5>
         <ul>
           <li>Front-end Development </li>
